1449ea1f71f3a9053de49a366719802eb2a75823
Deefoozy <tinotrok@outlook.com>
2025-05-27 15:48:01 +0200

    Refactored rendering code into ShipRenderer class

--- test/src/App.svelte
+++ test/src/App.svelte
@@ -16,13 +16,19 @@
       {
         name: "Main grid",
         position: new Vector3(0, 0, 0),
+        centerX: true,
+        centerY: false,
+        centerZ: false,
         sizeX: 4,
         sizeY: 4,
         sizeZ: 4,
       },
       {
         name: "Dropseat Grid",
-        position: new Vector3(-2, 0, 1),
+        position: new Vector3(0, 0, -4),
+        centerX: true,
+        centerY: false,
+        centerZ: false,
         sizeX: 1,
         sizeY: 3,
         sizeZ: 3,
@@ -30,79 +36,171 @@
     ],
   };
 
+  class ShipRenderer {
+      gridInfo = {}
+      renderedGridInfo = []
 
-  const renderer = new ThreeJs.WebGLRenderer();
-  renderer.setSize(width, height)
+      canvas = null
+      renderer = null
+      camera = null
+      controls = null
 
-  onMount(() => {
-    const canvasElement = document.getElementById("app_container")
+      width = null
+      height = null
 
-    canvasElement.appendChild(renderer.domElement)
-
-    // const camera = new ThreeJs.OrthographicCamera(10, 10, 10, 10, 1, 100);
-    const camera = new ThreeJs.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    camera.position.set(1.5, 1.5, 10)
-
-    // Set up FreqVars
-    const scene = new ThreeJs.Scene();
-    const material = new ThreeJs.MeshBasicMaterial({ color: 0xaaffaa });
-
-    // Set up lighting
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new ThreeJs.AmbientLight(color, intensity);
-    scene.add(light);
-
-    // Set up camera controls
-    const controls = new OrbitControls(camera, canvasElement);
-    controls.target.set(0, 5, 0);
-    controls.update();
-
-    const renderedGridInformation = [];
-
-    for (let i = 0, c = dataJson.grids.length; i < c; ++i) {
-      const BaseInfo = dataJson.grids[i];
-
-      // create grid obj
-      const gridInfo = {
-        name: BaseInfo.name,
-        boxes: [],
-      };
-
-      const boxAmt = BaseInfo.sizeX * BaseInfo.sizeY * BaseInfo.sizeZ;
-      const layerBoxAmount = BaseInfo.sizeX * BaseInfo.sizeY;
-
-      // loop through boxes and add to rGI object that was pushed
-      for (let i = 0; i < boxAmt; ++i) {
-        const tempBox = new ThreeJs.BoxGeometry(boxSize, boxSize, boxSize);
-        const boxObject = new ThreeJs.Mesh(tempBox, material);
-
-        const boxNumber = i + 1;
-
-        // figure out box pos
-        const rawPosX = (boxNumber % BaseInfo.sizeX) - 1;
-        const xRowsCompleted = Math.floor(i / BaseInfo.sizeX);
-
-        const posX = rawPosX < 0 ? BaseInfo.sizeX - 1 : rawPosX;
-        const posY = (xRowsCompleted % BaseInfo.sizeY);
-        const posZ = (Math.floor(i / layerBoxAmount));
-
-        boxObject.position.set(posX, posY, posZ);
-        boxObject.position.add(BaseInfo.position)
-
-        scene.add(boxObject);
-        gridInfo.boxes.push(boxObject);
+      /**
+       * @param {HTMLElement} parentElement
+       * @param {ThreeJs.WebGLRenderer} renderer
+       * @param {ThreeJs.PerspectiveCamera} camera
+       */
+      constructor(parentElement, renderer, camera) {
+          this.canvas = parentElement;
+          this.renderer = renderer;
+          this.camera = camera;
+          this.scene = new ThreeJs.Scene();
       }
 
-      // push to rGI for later reference
-      renderedGridInformation.push(gridInfo)
-    }
+      /**
+       * @param {{shipName: string, hangarSize: string, cargoSize: number, grids: {name: string, position: Vector3, centerX: boolean, centerY: boolean, centerZ: boolean, sizeX: number, sizeY: number, sizeZ: number}[]}} gridInformation
+       */
+      start(gridInformation) {
+          console.log("Starting up!")
+          this.setupLighting();
+          this.setupCamera();
+          this.setSize(width, height);
+          this.setupCanvas();
 
-    function animate() {
-      renderer.render( scene, camera );
-    }
-    renderer.setAnimationLoop( animate );
+          this.renderer.setAnimationLoop( () => {this.animate()} );
+
+          this.loadGridInfo(gridInformation);
+      }
+
+      /**
+       * @param {{shipName: string, hangarSize: string, cargoSize: number, grids: {name: string, position: Vector3, centerX: boolean, centerY: boolean, centerZ: boolean, sizeX: number, sizeY: number, sizeZ: number}[]}} gridInformation
+       * @param reRender
+       */
+      loadGridInfo(gridInformation, reRender = false) {
+          console.log("loadGridInfo!")
+          this.gridInfo = gridInformation
+
+          if (reRender) {
+              console.log("re_render")
+              this.clearModels()
+          }
+
+          this.createModels()
+      }
+
+      clearModels() {
+          console.error("NOT IMPLEMENTED")
+      }
+
+      /**
+       * @param {{name: string, position: Vector3, centerX: boolean, centerY: boolean, centerZ: boolean, sizeX: number, sizeY: number, sizeZ: number}} gridInformation
+       */
+      determineGridOffsetVector(gridInformation) {
+          let offsetX = 0;
+          let offsetY = 0;
+          let offsetZ = 0;
+
+          if (gridInformation.centerX && gridInformation.centerX === true) {
+              offsetX -= gridInformation.sizeX * 0.5
+          }
+
+          return new Vector3(offsetX, offsetY, offsetZ)
+      }
+
+      createModels() {
+          for (let i = 0, c = this.gridInfo.grids.length; i < c; ++i) {
+              const baseInfo = this.gridInfo.grids[i];
+
+              // create grid obj
+              const gridInfo = {
+                  name: baseInfo.name,
+                  offset: this.determineGridOffsetVector(baseInfo),
+                  boxes: [],
+              };
+
+              const boxAmt = baseInfo.sizeX * baseInfo.sizeY * baseInfo.sizeZ;
+              const layerBoxAmount = baseInfo.sizeX * baseInfo.sizeY;
+
+              // loop through boxes and add to rGI object that was pushed
+              for (let i = 0; i < boxAmt; ++i) {
+                  const tempBox = new ThreeJs.BoxGeometry(boxSize, boxSize, boxSize);
+                  const boxObject = new ThreeJs.Mesh(tempBox, new ThreeJs.MeshBasicMaterial({ color: 0xaaffaa }));
+
+                  const boxNumber = i + 1;
+
+                  // figure out box pos
+                  const rawPosX = (boxNumber % baseInfo.sizeX) - 1;
+                  const xRowsCompleted = Math.floor(i / baseInfo.sizeX);
+
+                  const posX = rawPosX < 0 ? baseInfo.sizeX - 1 : rawPosX;
+                  const posY = (xRowsCompleted % baseInfo.sizeY);
+                  const posZ = (Math.floor(i / layerBoxAmount));
+
+                  boxObject.position.set(posX, posY, posZ);
+                  boxObject.position.add(baseInfo.position);
+                  boxObject.position.add(gridInfo.offset)
+
+                  this.scene.add(boxObject);
+                  gridInfo.boxes.push(boxObject);
+              }
+
+              // push to rGI for later reference
+              this.renderedGridInfo.push(gridInfo)
+          }
+      }
+
+      setupCanvas() {
+          console.log("setupCanvas!")
+          this.canvas.appendChild(this.renderer.domElement)
+      }
+
+      setupLighting() {
+          console.log("setupLighting!")
+          const color = 0xFFFFFF;
+          const intensity = 1;
+          this.scene.add(new ThreeJs.AmbientLight(color, intensity));
+      }
+
+      setupCamera() {
+          console.log("setupCamera!")
+          this.camera.position.set(1.5, 1.5, 10)
+
+          this.controls = new OrbitControls(this.camera, this.canvas);
+          this.controls.target.set(0, 5, 0);
+          this.controls.update();
+      }
+
+      /**
+       * @param {number} width
+       * @param {number} height
+       */
+      setSize(width, height) {
+          console.log("setSize!")
+          this.width = width
+          this.height = height
+
+          this.renderer.setSize(width, height)
+      }
+
+      animate() {
+          this.renderer.render( this.scene, this.camera );
+      }
+  }
+
+  onMount(() => {
+    console.log("hello world")
+
+    const canvasElement = document.getElementById("app_container")
+    const renderer = new ThreeJs.WebGLRenderer();
+    // const camera = new ThreeJs.OrthographicCamera(10, 10, 10, 10, 1, 100);
+    const camera = new ThreeJs.PerspectiveCamera(80, width / height, 0.1, 1000);
+
+    const shipRenderer = new ShipRenderer(canvasElement, renderer, camera);
+
+    shipRenderer.start(dataJson)
   })
 </script>
 
